6bd8d589577083d8fd4807d2608aabd0fadaf151
zhangruyin <zhangruyin@zhangruyins-MacBook-Pro.local>
2017-11-19 17:10:34 -0500

    added AuthenticatorTest.java

--- Application/test/controllers/AuthenticatorTest.java
+++ Application/test/controllers/AuthenticatorTest.java
@@ -2,56 +2,65 @@ package controllers;
 import controllers.Authenticator;
 import static org.junit.Assert.*;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+
 public class AuthenticatorTest {
-	FileWriter fw;
-	BufferedWriter bf;
-	StringBuilder sb;
-
-	public void setUp() {	
-		try {
-			fw = new FileWriter("users.csv", true);
-			bf = new BufferedWriter(fw);
-			sb = new StringBuilder();
-			sb.append("TRUE,julian.barker@mail.utoronto.ca,a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3,Julian	Barker");
-			sb.append("FALSE,ruyin.zhang@mail.utoronto.ca,a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3,Olivia	Zhang,1002205883");
-			sb.append("TRUE,tito.ku@mail.utoronto.ca,a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3,Tito Ku");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+	static File f;
+	static FileWriter fw;
+	static StringBuilder sb;
+	static BufferedWriter bf;
+	
 
 
+	@BeforeClass
+	public static void setUp() throws IOException {
+		TestFileHandler.setUp();
+		
+		f = new File("users.csv");
+		fw = new FileWriter(f.getName());
+		bf = new BufferedWriter(fw);
+		sb = new StringBuilder();
+		sb.append("TRUE,julian.barker@mail.utoronto.ca,a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3,Julian	Barker");
+		sb.append("\n");
+		sb.append("FALSE,ruyin.zhang@mail.utoronto.ca,a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3,Olivia	Zhang,1002205883");
+		sb.append("\n");
+		sb.append("TRUE,tito.ku@mail.utoronto.ca,a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3,Tito Ku");
+		bf.write(sb.toString());
+		bf.close();
+	}	
 
+
+	/**
+	 * Test whether the user is a instructor or not by given the user's email
+	 * return true when the user is an instructor
+	 */
 	@Test
 	public void testisInstructor() {
 		String email = "julian.barker@mail.utoronto.ca";
-		try {
-			bf.write(sb.toString());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		assertTrue(Authenticator.isInstructor(email));
 	}
 	
+	/**
+	 * Test whether the user is a instructor or not by given the user's email
+	 * return false when the user is an instructor
+	 */
 	@Test
 	public void testIsNotInstructor() {
 		String email = "ruyin.zhang@mail.utoronto.ca";
-		try {
-			bf.write(sb.toString());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		assertFalse(Authenticator.isInstructor(email));
 	}
 	
+	/**
+	 * Test whether the user enters a correct user name and password to login
+	 * return true when the user provides correct user name and password
+	 */
 	@Test
 	public void testAuthenticate() {
 		String email = "ruyin.zhang@mail.utoronto.ca";
@@ -59,6 +68,10 @@ public class AuthenticatorTest {
 		assertTrue(Authenticator.authenticate(email, password));
 	}
 	
+	/**
+	 * Test whether the user enters a correct user name and password to login
+	 * return false when the user provides incorrect user name and password
+	 */
 	@Test
 	public void testFailAuthenticate() {
 		String email = "ruyin.zhang@mail.utoronto.ca";
@@ -68,7 +81,8 @@ public class AuthenticatorTest {
 	
 	@Test
 	public void tearDown() {
-		
+		f.delete();		
+		TestFileHandler.tearDown();
 	}
 	
 	
