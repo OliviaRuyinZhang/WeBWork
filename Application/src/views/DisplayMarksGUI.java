142b3bb9519d640bd8925e2db412fd41ef4bdbf5
julianb393 <julian.barker@mail.utoronto.ca>
2017-11-19 02:03:07 -0500

    committing current state.

--- Application/src/views/DisplayMarksGUI.java
+++ Application/src/views/DisplayMarksGUI.java
@@ -1,10 +1,17 @@
 package views;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
 
 public class DisplayMarksGUI extends JFrame{
+
+	private JPanel contentPane;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -25,5 +32,24 @@ public class DisplayMarksGUI extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(400,500);
 		setLocationRelativeTo(null);
+		
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setLayout(new GridBagLayout());	
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.anchor = GridBagConstraints.NORTH;
+		gbc.fill = GridBagConstraints.CENTER;
+	    gbc.weighty = 1;
+	    
+	    gbc.gridx = 0;
+	    gbc.gridy = 0;
+	    
+	    JList<String> lstStudentList = new JList<String>();
+	    
+	    
+	    contentPane.add(lstStudentList);
+	    
+	    add(contentPane);
 	}
 }
