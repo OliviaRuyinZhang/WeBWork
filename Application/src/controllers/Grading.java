d4ae6cdd51f6415e44099db558ea9bb1482c2882
Felix Chen <Me@Felixs-MacBook.local>
2017-11-19 16:37:41 -0500

    Fixed grading class, now checks for all assignment submission files

--- Application/src/controllers/Grading.java
+++ Application/src/controllers/Grading.java
@@ -4,46 +4,66 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Grading {
 	
-	/**
-	 * Returns a HashMap of the students grades per assignment number, given
-	 * the student's id.
-	 * @param id Student number
-	 * @return HashMap<Integer, String>
-	 */
-        public static HashMap<Integer, String> getStudentGrades(String id){
-        	HashMap<Integer, String> assignNumToGrade = new HashMap<>();
-                String submissionFileName = "";
-                int i = 1;
-                
-                // Get all assignment submission files
-                while((new File(submissionFileName = ("Assignment" + Integer.toString(i) + "Submission.csv")).exists())){
-                        try {
-                                FileReader fr = new FileReader(submissionFileName);
-                                BufferedReader br = new BufferedReader(fr);
-                                // Skip first cell
-                                String line = br.readLine();
-                                // Read all studentIDs
-                                while ((line = br.readLine()) != null) {
-                                        // Get line
-                                        String[] assignmentLine = line.split(",");
-                                        // Check if studentID is present
-                                        if (assignmentLine[0].equals(id)){
-                                                // Last index == length - 1
-                                        		assignNumToGrade.put(i, assignmentLine[assignmentLine.length - 1] + "%");
-                                                break;
-                                        }
-                                }
-                                br.close();
-                                fr.close();
-                        } catch (IOException e) {
-                                e.printStackTrace();
-                        }
-                        i++;
+    /**
+     * Returns a HashMap of the students grades per assignment number, given
+     * the student's id.
+     * @param id Student number
+     * @return HashMap<Integer, String>
+     */
+    public static HashMap<Integer, String> getStudentGrades(String id){
+            HashMap<Integer, String> assignNumToGrade = new HashMap<>();
+            int i = 1;
+            
+            for (File f: getExistingAssignments()){
+                try {
+                    FileReader fr = new FileReader(f.getName());
+                    BufferedReader br = new BufferedReader(fr);
+                    // Skip first cell
+                    String line = br.readLine();
+                    // Read all studentIDs
+                    while ((line = br.readLine()) != null) {
+                            // Get line
+                            String[] assignmentLine = line.split(",");
+                            // Check if studentID is present
+                            if (assignmentLine[0].equals(id)){
+                                    // Last index == length - 1
+                                    assignNumToGrade.put(i, assignmentLine[assignmentLine.length - 1] + "%");
+                                    break;
+                            }
+                    }
+                    br.close();
+                    fr.close();
+                } catch (IOException e) {
+                        e.printStackTrace();
                 }
-                return assignNumToGrade;
+                i++;
+            }
+            return assignNumToGrade;
         }
+        
+        private static ArrayList<File> getExistingAssignments(){
+		
+            ArrayList<File> assignments = new ArrayList<>();
+            Pattern pattern= Pattern.compile("Assignment+(\\d)*+Submission.csv");
+            
+            Matcher matcher;
+	    
+	    File[] files = new File(".").listFiles(); // All files in current directory.
+	    for(File file: files) {
+	    	if(file.isFile()) {
+	    		matcher = pattern.matcher(file.getName());
+	    		if(matcher.find()) { // If file name matches the regex expression in pattern.
+	    			assignments.add(file);
+	    		}
+	    	}
+	    }  
+        return assignments;
+    }
 }
