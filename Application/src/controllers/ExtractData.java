29b48a56d9181c0c513648770a3187b799b70a57
Felix Chen <Me@Felixs-MacBook.local>
2017-11-24 22:52:32 -0500

    Moved extractfeedback function to extractdata

--- Application/src/controllers/ExtractData.java
+++ Application/src/controllers/ExtractData.java
@@ -6,7 +6,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
+import models.Feedback;
 
 public class ExtractData {
 
@@ -281,4 +284,28 @@ public class ExtractData {
 
 		return studentID;
 	}
+        public static ArrayList<Feedback> getAssignmentFeedback(String fileName){
+        
+        ArrayList<Feedback> f = new <Feedback>ArrayList();
+        String line = null;
+        
+        try {
+            FileReader fr = new FileReader(fileName);
+            BufferedReader br = new BufferedReader(fr);
+            while ((line = br.readLine()) != null) {
+                    // Seperate date, feedback
+                    String[] lineSplit = line.split(",");
+                    // Seperate dd, mm, yyyy
+                    String[] dateSplit = lineSplit[0].split("/");
+                    Calendar date = new GregorianCalendar(Integer.parseInt(dateSplit[2]), 
+                            Integer.parseInt(dateSplit[1]), Integer.parseInt(dateSplit[0]));
+                    f.add(new Feedback(lineSplit[1], date));
+            }
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
 }
