8813cf5dcabea42370d331584e2b9a3c749605a5
DeedoeGuu <tikuto@hotmail.com>
2017-11-26 03:18:35 -0500

    Fixed sizing of check/cross images to be used. Finished base StudentSubmissionSummaryGUI.java. Need to fix scroll bar as well as link it up to whole application.

--- Application/src/views/StudentSubmissionSummaryGUI.java
+++ Application/src/views/StudentSubmissionSummaryGUI.java
@@ -9,34 +9,41 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
 import controllers.ExtractData;
 import models.Problem;
 
 public class StudentSubmissionSummaryGUI extends JFrame {
-	protected static final int PANEL_GAP = 90; //the constant gap inbetween all questions
+	protected static final int PANEL_GAP = 100; //the constant gap in between all questions
 	private JPanel contentPane;
+	private String email;
+	private File file;
 	
-	public StudentSubmissionSummaryGUI(String file){
-		
-		//scroll bar
-		JScrollPane scroll = new JScrollPane(contentPane, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scroll.getVerticalScrollBar().setUnitIncrement(16);
+	public StudentSubmissionSummaryGUI(File file, String email){
+		this.email = email;
+		this.file = file;		
 		
 		//main content pane
 		setResizable(false);
-		setSize (600, 600);
+		setSize(600,600);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		contentPane = new JPanel();
+		//scroll bar
+		JScrollPane scroll = new JScrollPane(contentPane,
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scroll.getVerticalScrollBar().setUnitIncrement(16);
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		scroll.setBounds(0, 0, 600, 600);
 		getContentPane().add(scroll);
-		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		//summary label
@@ -46,37 +53,97 @@ public class StudentSubmissionSummaryGUI extends JFrame {
 		lblSummary.setSize(lblSummary.getPreferredSize());
 		contentPane.add(lblSummary);
 		
+		//setting the check mark label
+		Icon checkMark = new ImageIcon("resources/check_mark.png");
+		JLabel lblCheck;
+		//setting the x mark label
+		Icon xMark = new ImageIcon("resources/cross_mark.png");
+		JLabel lblX;
+		ArrayList<Problem> problems = getProblems(file.getName());
+		ArrayList<Boolean> summary = checkRightWrong(problems);
+		int ycoord = 0;
+		JPanel problemPanel;
 		//create a panel for each question
-		ArrayList<Problem> problems = new ArrayList<>();
-		problems = getProblems(file);
-		for (Problem p : problems) {
-			int ycoord = 0;
-			JPanel problemPanel = new JPanel();
+		for (int i = 0; i < problems.size(); i++) {
+			problemPanel = new JPanel();
 			problemPanel.setLayout(null);
 			problemPanel.setBackground(Color.decode("#F0F0F0"));
 			problemPanel.setBorder(javax.swing.BorderFactory.createEmptyBorder());
-			problemPanel.setBounds(0, ycoord + PANEL_GAP, 600, 85);
+			problemPanel.setBounds(12, ycoord + PANEL_GAP, 555, 85);
 			ycoord += PANEL_GAP;
-//			addToProblemPanel(problemPanel, fileName);
+			addToProblemPanel(problemPanel, problems.get(i));
+			//if student got it right, check mark is shown
+			if (summary.get(i)) {
+				lblCheck = new JLabel(checkMark);
+				lblCheck.setBounds(500, 20, 50, 50);
+				problemPanel.add(lblCheck);
+			} else { //if student got it wrong, x mark is shown
+				lblX = new JLabel(xMark);
+				lblX.setBounds(500, 20, 50, 50);
+				problemPanel.add(lblX);
+			}
 			contentPane.add(problemPanel);
 		}
 	}
 	
 	/**
+	 * Checks if the students answer is right or wrong with the corresponding problems' solution
+	 * @param ArrayList<Problem> p which is the ArrayList of Problem objects to be used in
+	 * comparing between right/wrong
+	 * @return ArrayList<Boolean> where it holds if student got right/wrong (true/false respectively)
+	 * to each question where each index is the respective question
+	 */
+	private ArrayList<Boolean> checkRightWrong(ArrayList<Problem> p) {
+		ArrayList<Boolean> summary = new ArrayList<>();
+		String fileName = this.file.getName();
+		String submissionName = fileName.substring(0, fileName.indexOf(".")) + "Submission.csv";
+		try {
+			FileReader fr = new FileReader(submissionName);
+			BufferedReader br = new BufferedReader(fr);
+			br.readLine();
+			String stuNum = ExtractData.getStudentID(this.email);
+			String line = br.readLine();
+			String[] info = line.split(",");
+			//read until desired student number is found
+			while ((line = br.readLine()) != null) {
+				info = line.split(",");
+				if (info[0].equals(stuNum)) {
+					break;
+				}
+			}
+			br.close();
+			fr.close();
+			for (int i = 0; i < p.size(); i++) {
+				if(p.get(i).getSolution().equals(info[i+1])){
+					summary.add(true);
+				} else {
+					summary.add(false);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return summary;
+	}
+		
+	/**
 	 * Adds question data to the designated panel to be shown
 	 * @param JPanel panel to which question info is to be added to
-	 * @param File file where questions are located
+	 * @param Problem p the problem object
 	 */
-	private void addToProblemPanel(JPanel panel, File file) {
-		ArrayList<ArrayList<String>> qData = ExtractData.getAssignmentQData(file);
+	private void addToProblemPanel(JPanel panel, Problem p) {
 		
-		//show question number
-		for (int i = 0; i < qData.size(); i++) {
-			JLabel lblQuestion = new JLabel(qData.get(0).get(i));
-			lblQuestion.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-			lblQuestion.setBounds(50, -3, 600, 85);
-			panel.add(lblQuestion);
-		}
+		JTextArea txtProblem = new JTextArea(p.getProblemString());
+		txtProblem.setFont(new Font("Segoe UI Light", Font.PLAIN, 16));
+		txtProblem.setEditable(false);
+		txtProblem.setLineWrap(true);
+		txtProblem.setWrapStyleWord(true);
+		txtProblem.setSize(txtProblem.getPreferredSize());
+		
+		JScrollPane scroll = new JScrollPane(txtProblem, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scroll.setBounds(15, 14, 480, 60);
+		panel.add(scroll);
 	}
 	
 	/**
@@ -114,7 +181,7 @@ public class StudentSubmissionSummaryGUI extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					StudentSubmissionSummaryGUI frame = new StudentSubmissionSummaryGUI("Assignment1.csv");
+					StudentSubmissionSummaryGUI frame = new StudentSubmissionSummaryGUI(new File("Assignment1.csv"), "stu");
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
